3fa45a0a08c93aef09efaf70ecb05a6d6ce7ec1b
KUMIR2006 <kurbanovmirzobek@gmail.com>
2026-03-31 22:27:52 +0300

    feat: add responsive navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,4 @@
 const Homepage = () => {
-  return (
-    <div className=''>Homepage</div>
-  )
-}
-
-export default Homepage
+  return <div className="">Homepage</div>;
+};
+export default Homepage;
